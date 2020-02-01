3e3647b49f55e4a9e66c1f6a206f22e234f416bf
kaustav1616 <41225791+kaustav1616@users.noreply.github.com>
2020-02-01 22:31:53 +0530

    Update MATRIX2.cpp

--- MATRIX2.cpp
+++ MATRIX2.cpp
@@ -35,6 +35,7 @@ long int num_submatrices(vector<vector<long int>> &input)
     int i, j;
     long int count = 0;
     
+    /* input[i][j] stores longest continuous column of '1's extending downward starting at (i, j) */
     for(j = 0; j < input[0].size(); ++j)
     {
         for(i = input.size() - 1; i >= 0; --i)
@@ -49,6 +50,15 @@ long int num_submatrices(vector<vector<long int>> &input)
         }
     }
     
+    /* input[i][j] will store number of sub-matrices with (i, j) as top right corner.
+     * At (i, j), before calculation, input[i][j - 1] stores number of sub-matrices with 
+     * (i, j - 1) as top right corner and input[i][j] stores longest continuous column of
+     * '1's extending downward starting at (i, j). The latter can be thought of as the number
+     * of submatrices that (i, j) is CAPABLE of producing with itself as the top-right corner;
+     * the former can be thought of as a limit on the number of submatrices (i, j) is ALLOWED
+     * to produce with itself as itself as the top right corner. Thus, the calculated value
+     * of input[i][j] is the minimum of the two.
+     */
     for(i = 0; i < input.size(); ++i)
     {
         for(j = 0; j < input[i].size(); ++j)
